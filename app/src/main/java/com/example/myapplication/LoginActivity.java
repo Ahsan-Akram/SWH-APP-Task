eb81ed709a50f34f5affb4ac013dd8c4880f2640
Ahsan-Akram <63956204+Ahsan-Akram@users.noreply.github.com>
2023-07-11 18:29:05 +0500

    Ui updated of booking activity

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
@@ -14,18 +15,35 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
         TextView signup = findViewById(R.id.signup);
+        Button singinBtn = findViewById(R.id.signin_btn);
+        TextView emailView = findViewById(R.id.login_email);
+        TextView passwordView = findViewById(R.id.login_password);
+
+
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 launchSignUpActivity();
             }
         });
+
+        singinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+    }
+
+    private void signIn() {
+
+        Intent intent = new Intent(this, OnDemandBookingActivity.class);
+        startActivity(intent);
     }
 
     private void launchSignUpActivity() {
-        Intent signupIntent = new Intent(this,SignupActivity.class);
+        Intent signupIntent = new Intent(this, SignupActivity.class);
         startActivity(signupIntent);
     }
 }
